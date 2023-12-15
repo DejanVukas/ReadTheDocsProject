b3095bda19f948b0d8a79451de48c642f9b688ac
DejanVukas <dejan.vukas@outlook.com>
2023-12-15 18:48:32 +0100

    Changed conf.py (sphinx_rtd_theme related error)

--- conf.py
+++ conf.py
@@ -9,7 +9,8 @@
 #I HAVE ADDED THIS SECTION BECAUSE SPHINX_RTD_THEME COULD NOT BE FOUND
 import os
 import sys
-sys.path.insert(0, os.path.abspath('C:\Users\Vukas\AppData\Local\Programs\Python\Python311\Lib\site-packages'))
+#error occurs because you are using a normal string as a path. You can put r before your normal string. It converts a normal string to a raw string
+sys.path.insert(0, os.path.abspath(r'C:\Users\Vukas\AppData\Local\Programs\Python\Python311\Lib\site-packages'))
 import sphinx_rtd_theme
 #END OF MY CHANGES
 
